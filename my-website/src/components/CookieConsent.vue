1b0560e60ab0b70ac6c8e29741be51b72575b2d8
dusan-krstic <dusan.krstic@ingsoftware.com>
2019-10-21 11:12:51 +0200

    init commit

---
+++ my-website/src/components/CookieConsent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="cookie-consent">
+    <p>Some yada yada text about cookie policy</p>
+    <button @click="enableAnalytics">I agree</button>
+    <button @click="disableAnalytics">No, thanks</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cookieConsent',
+  methods: {
+    enableAnalytics () {
+      this.$ga.enable()
+      localStorage.setItem('isAnalyticLoaded', 'true')
+    },
+    disableAnalytics () {
+      this.$ga.disable()
+      localStorage.setItem('isAnalyticLoaded', 'false')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .cookie-consent {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    background-color: bisque;
+    text-align: center;
+    z-index: 150;
+  }
+</style>
